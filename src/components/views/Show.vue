17f286cd851a149af62a7dbd89436c822c44d83a
xiaojingdou2016 <yuchan_yang@126.com>
2018-07-09 20:11:52 +0800

    演出

--- src/components/views/Show.vue
+++ src/components/views/Show.vue
@@ -1,10 +1,203 @@
 <template>
-	<div class="wrapper">
-		演出
+	<div class="show-wrapper">
+		<header class="show-header">
+			<h2 class="show-title">演出</h2>
+		</header>
+		<div class="location-search">
+				<span class="loaction">上海<i class="icon iconfont icon-down"></i></span>
+				<div class="search">
+					<i class="icon iconfont icon-search"></i>
+					<input type="text" placeholder="搜索明星、演出" class="control"/>
+				</div>
+		</div>
+		<section class="show-content">
+			<div class="swiper-box">
+				<tab :line-width="2" custom-bar-width="30px" v-model="index">
+				<tab-item :selected="actived === index" v-for="(item, index) in navTab" @click="actived = index" :key="index">{{item}}</tab-item>
+			</tab>
+			</div>
+			<div class="recommend" v-show="index===0">
+				<swiper :list="demo03_list" style="width:100%;margin:0 auto;" :aspect-ratio="295/720"></swiper>
+				<div class="must-see">
+					<h2 class="box-title">必看清单</h2>
+					<ul class="menu-list">
+						<li class="must-item" v-for="i in 6">
+							<div class="item-pic">
+								<img src="../../../static/img/movie-pic2.jpg" alt="" />
+								<span class="num">{{i}}</span>
+							</div>
+							<h3 class="item-name">陈佩斯戏剧作品展：戏台</h3>
+							<p class="item-info">10月·当代舞台喜剧的有一座高峰</p>
+						</li>
+					</ul>
+				</div>
+				<div class="hot-recommend">
+					<div class="hot-header">
+					<h2 class="box-title">热门推荐</h2>
+					<span class="loaction">推荐排序<i class="icon iconfont icon-down"></i></span>
+					</div>
+					<ul class="hot-list">
+						<li class="hot-item" v-for="i in 6">
+							<div class="item-pic">
+								<img src="../../../static/img/movie-pic.jpg" alt="" />
+								<span class="num">￥298起</span>
+							</div>
+							<h3 class="item-name">陈佩斯戏剧作品展：戏台</h3>
+							<p class="item-info">10月·当代舞台喜剧的有一座高峰</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		<!--演唱会-->
+		<div class="recommend" v-show="index===1">
+				<div class="hot-recommend concert-list">
+					<div class="hot-header concert-header">
+					<span class="loaction">全部时间<i class="icon iconfont icon-down"></i></span>
+					<span class="loaction">推荐排序<i class="icon iconfont icon-down"></i></span>
+					</div>
+					<ul class="hot-list">
+						<li class="hot-item" v-for="i in 6">
+							<div class="item-pic">
+								<img src="../../../static/img/movie-pic3.jpg" alt="" />
+								<span class="num">￥298起</span>
+							</div>
+							<h3 class="item-name">陈佩斯戏剧作品展：戏台</h3>
+							<p class="item-info">10月·当代舞台喜剧的有一座高峰</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<!--歌剧话剧-->
+			<div class="recommend" v-show="index===2">
+				<div class="hot-recommend concert-list">
+					<div class="hot-header concert-header">
+					<span class="loaction">全部时间<i class="icon iconfont icon-down"></i></span>
+					<span class="loaction">推荐排序<i class="icon iconfont icon-down"></i></span>
+					</div>
+					<ul class="hot-list">
+						<li class="hot-item" v-for="i in 6">
+							<div class="item-pic">
+								<img src="../../../static/img/movie-pic4.jpg" alt="" />
+								<span class="num">￥298起</span>
+							</div>
+							<h3 class="item-name">陈佩斯戏剧作品展：戏台</h3>
+							<p class="item-info">10月·当代舞台喜剧的有一座高峰</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<!--歌剧话剧-->
+			<div class="recommend" v-show="index===3">
+				<div class="hot-recommend concert-list">
+					<div class="hot-header concert-header">
+					<span class="loaction">全部时间<i class="icon iconfont icon-down"></i></span>
+					<span class="loaction">推荐排序<i class="icon iconfont icon-down"></i></span>
+					</div>
+					<ul class="hot-list">
+						<li class="hot-item" v-for="i in 6">
+							<div class="item-pic">
+								<img src="../../../static/img/movie-pic.jpg" alt="" />
+								<span class="num">￥298起</span>
+							</div>
+							<h3 class="item-name">陈佩斯戏剧作品展：戏台</h3>
+							<p class="item-info">10月·当代舞台喜剧的有一座高峰</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<!--歌剧话剧-->
+			<div class="recommend" v-show="index===4">
+				<div class="hot-recommend concert-list">
+					<div class="hot-header concert-header">
+					<span class="loaction">全部时间<i class="icon iconfont icon-down"></i></span>
+					<span class="loaction">推荐排序<i class="icon iconfont icon-down"></i></span>
+					</div>
+					<ul class="hot-list">
+						<li class="hot-item" v-for="i in 6">
+							<div class="item-pic">
+								<img src="../../../static/img/movie-pic2.jpg" alt="" />
+								<span class="num">￥298起</span>
+							</div>
+							<h3 class="item-name">陈佩斯戏剧作品展：戏台</h3>
+							<p class="item-info">10月·当代舞台喜剧的有一座高峰</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<!--歌剧话剧-->
+			<div class="recommend" v-show="index===5">
+				<div class="hot-recommend concert-list">
+					<div class="hot-header concert-header">
+					<span class="loaction">全部时间<i class="icon iconfont icon-down"></i></span>
+					<span class="loaction">推荐排序<i class="icon iconfont icon-down"></i></span>
+					</div>
+					<ul class="hot-list">
+						<li class="hot-item" v-for="i in 6">
+							<div class="item-pic">
+								<img src="../../../static/img/movie-pic3.jpg" alt="" />
+								<span class="num">￥298起</span>
+							</div>
+							<h3 class="item-name">陈佩斯戏剧作品展：戏台</h3>
+							<p class="item-info">10月·当代舞台喜剧的有一座高峰</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<!--歌剧话剧-->
+		</section>
 	</div>
 </template>
 
 <script>
+	import {
+		Tab,
+		TabItem,
+		Swiper,
+		SwiperItem,
+		ButtonTab, 
+		ButtonTabItem, 
+		Divider,
+		Scroller
+	} from 'vux'
+
+	const list = () => ['推荐', '演唱会','歌剧话剧', '亲子演出','音乐会', '戏曲艺术']
+	export default {
+		name: "Movie",
+		components: {
+			Tab,
+			TabItem,
+			Swiper,
+			SwiperItem,
+			ButtonTab, 
+			ButtonTabItem, 
+			Divider,
+			Scroller,
+		},
+		data() {
+			return {
+				actived: '推荐',
+				index: 0,
+				navTab: list(),
+				demo03_list:[{
+				  url: 'javascript:',
+				  img: '/static/img/banner1.png',
+				  title: '看大牌演出，首选大麦网',
+				},{
+				  url: 'javascript:',
+				  img: '/static/img/banner2.png',
+				  title: '看大牌演出，首选大麦网',
+				},{
+				  url: 'javascript:',
+				  img: '/static/img/banner3.png',
+				  title: '看大牌演出，首选大麦网',
+				}]
+			}
+		},
+		methods: {
+			consoleIndex(){
+			}
+		}
+	}
 </script>
 
 <style>
--- src/components/views/Video.vue
+++ src/components/views/Video.vue
@@ -1,11 +1,162 @@
 <template>
-	<div class="wrapper">
-		
+	<div class="video-wrapper">
+		<header class="video-menu">
+			<tab :line-width="2" custom-bar-width="30px" v-model="index">
+			<tab-item :selected="actived === index" v-for="(item, index) in navTab" @click="actived = index" :key="index">{{item}}</tab-item>
+		</tab>
+		</header>
+		<section>
+			<div class="tab-swiper vux-center" v-show="index===0">
+				<ul class="video-list clearfix">
+					<li class="video-item" v-for=" i in 8">
+						<div class="video-box">
+							<video width="100%" height="211" controls="controls">
+							  <source src="http://bizcommon.alicdn.com/KM596pxVraOsnEmgAP7/TAkYGi1FMi7sub718Gh%40%40hd.mp4" type="video/mp4">
+							</video>
+						</div>
+						<h2 class="video-name">《快把我哥带走》特辑番外 彭益畅孙泽阳变情侣</h2>
+						<div class="video-other">
+							<div class="tigs">影片速递</div>
+							<div class="">
+								<span><i class="icon iconfont icon-dianzan"></i> 赞</span>
+								<span><i class="icon iconfont icon-comment"></i> 3</span>
+								<span><i class="icon iconfont icon-share"></i></span>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<!--tab2-->
+			<div class="tab-swiper vux-center" v-show="index===1">
+				<ul class="video-list clearfix">
+					<li class="video-item" v-for=" i in 8">
+						<div class="video-box">
+							<video width="100%" height="211" controls="controls">
+							  <source src="http://bizcommon.alicdn.com/KM596pxVraOsnEmgAP7/TAkYGi1FMi7sub718Gh%40%40hd.mp4" type="video/mp4">
+							</video>
+						</div>
+						<h2 class="video-name">《我不是药神》特辑番外 徐峥王传君领衔主演</h2>
+						<div class="video-other">
+							<div class="tigs">影片速递</div>
+							<div class="">
+								<span><i class="icon iconfont icon-dianzan"></i> 赞</span>
+								<span><i class="icon iconfont icon-comment"></i> 3</span>
+								<span><i class="icon iconfont icon-share"></i></span>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<!--tab3-->
+			<div class="tab-swiper vux-center" v-show="index===2">
+				<ul class="video-list clearfix">
+					<li class="video-item" v-for=" i in 8">
+						<div class="video-box">
+							<video width="100%" height="211" controls="controls">
+							  <source src="http://bizcommon.alicdn.com/KM596pxVraOsnEmgAP7/TAkYGi1FMi7sub718Gh%40%40hd.mp4" type="video/mp4">
+							</video>
+						</div>
+						<h2 class="video-name">新大头儿子和小头爸爸3：俄罗斯奇遇记</h2>
+						<div class="video-other">
+							<div class="tigs">影片速递</div>
+							<div class="">
+								<span><i class="icon iconfont icon-dianzan"></i> 赞</span>
+								<span><i class="icon iconfont icon-comment"></i> 3</span>
+								<span><i class="icon iconfont icon-share"></i></span>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<!--tab4-->
+			<div class="tab-swiper vux-center" v-show="index===3">
+				<ul class="video-list clearfix">
+					<li class="video-item" v-for=" i in 8">
+						<div class="video-box">
+							<video width="100%" height="211" controls="controls">
+							  <source src="http://bizcommon.alicdn.com/KM596pxVraOsnEmgAP7/TAkYGi1FMi7sub718Gh%40%40hd.mp4" type="video/mp4">
+							</video>
+						</div>
+						<h2 class="video-name">《超人总动员2》特辑番外 </h2>
+						<div class="video-other">
+							<div class="tigs">影片速递</div>
+							<div class="">
+								<span><i class="icon iconfont icon-dianzan"></i> 赞</span>
+								<span><i class="icon iconfont icon-comment"></i> 3</span>
+								<span><i class="icon iconfont icon-share"></i></span>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<!--tab5-->
+			<div class="tab-swiper vux-center" v-show="index===4">
+				<ul class="video-list clearfix">
+					<li class="video-item" v-for=" i in 8">
+						<div class="video-box">
+							<video width="100%" height="211" controls="controls">
+							  <source src="http://bizcommon.alicdn.com/KM596pxVraOsnEmgAP7/TAkYGi1FMi7sub718Gh%40%40hd.mp4" type="video/mp4">
+							</video>
+						</div>
+						<h2 class="video-name">《快把我哥带走》特辑番外 彭益畅孙泽阳变情侣</h2>
+						<div class="video-other">
+							<div class="tigs">影片速递</div>
+							<div class="">
+								<span><i class="icon iconfont icon-dianzan"></i> 赞</span>
+								<span><i class="icon iconfont icon-comment"></i> 3</span>
+								<span><i class="icon iconfont icon-share"></i></span>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</section>
 	</div>
 </template>
 <script>
-	
+	import {
+		Tab,
+		TabItem,
+		Swiper,
+		SwiperItem,
+		ButtonTab, 
+		ButtonTabItem, 
+		Divider,
+		Scroller
+	} from 'vux'
+	const list = () => ['推荐', '速看','新片上映','剧集','综艺']
+	export default {
+		name: "Movie",
+		components: {
+			Tab,
+			TabItem,
+			Swiper,
+			SwiperItem,
+			ButtonTab, 
+			ButtonTabItem, 
+			Divider,
+			Scroller
+		},
+		data() {
+			return {
+				actived: '推荐',
+				index: 0,
+				navTab: list(),
+				film: 0
+			}
+		},
+		methods: {
+			consoleIndex(){
+			}
+		}
+	}
 </script>
+
+<style scoped="scoped">
+.movie-pic{
+	margin-bottom: 0.25rem;
+}
+</style>
 <style>
 	
 </style>
