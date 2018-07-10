8256dce619df18a773fa3c30df585e6c79918cf9
xiaojingdou2016 <yuchan_yang@126.com>
2018-07-10 18:26:47 +0800

    我的

--- src/components/views/User.vue
+++ src/components/views/User.vue
@@ -1,6 +1,80 @@
 <template>
-	<div class="wrapper">
-		我的
+	<div class="user-wrapper">
+		<header class="user-header">
+			<div class="set-remind">
+				<span class="set"><i class="icon iconfont icon-set"></i></span>
+				<span class="remind"><i class="icon iconfont icon-remind"></i></span>
+			</div>
+			<div class="user-info">
+				<div class="user-photo">
+					<img src="../../../static/img/photo.jpg"/>
+				</div>
+				<div class="user-msg">
+					<h2 class="user-name">小精豆847</h2>
+					<p class="attention">
+						<span>关注 0 | 被关注 0</span>
+						<span><i class="icon iconfont icon-arrow-right"></i></span>
+					</p>
+					<p class="rank"><i class="icon iconfont icon-crown"></i>白银会员</p>
+				</div>
+			</div>
+		</header>
+		<div class="user-core">
+			<span class="member-core">会员中心</span>
+			<span class="advertising">《风语咒》提前看<i class="icon iconfont icon-arrow-right"></i></span>
+		</div>
+		<section class="user-content">
+			<ul class="core-classify">
+				<li class="classify-item">
+					<p class="classify-ico"><i class="icon iconfont icon-film"></i></p>
+					<p class="classify-name">电影票</p>
+				</li>
+				<li class="classify-item">
+					<p class="classify-ico"><i class="icon iconfont icon-youhuiquan"></i></p>
+					<p class="classify-name">1张优惠券</p>
+				</li>
+				<li class="classify-item">
+					<p class="classify-ico"><i class="icon iconfont icon-jiuzhenqia"></i></p>
+					<p class="classify-name">权益卡</p>
+				</li>
+				<li class="classify-item">
+					<p class="classify-ico"><i class="icon iconfont icon-show"></i></p>
+					<p class="classify-name">演出票</p>
+				</li>
+			</ul>
+			<div class="other">
+				<span class="member-core">想看的电影</span>
+				<span class="advertising">1<i class="icon iconfont icon-arrow-right"></i></span>
+			</div>
+			<div class="other">
+				<span class="member-core">看过的电影</span>
+				<span class="advertising">12<i class="icon iconfont icon-arrow-right"></i></span>
+			</div>
+			<div class="other">
+				<span class="member-core">我的西虹市市民身价</span>
+				<span class="advertising"><i class="icon iconfont icon-arrow-right"></i></span>
+			</div>
+			<div class="other">
+				<span class="member-core">发起的讨论</span>
+				<span class="advertising"><i class="icon iconfont icon-arrow-right"></i></span>
+			</div>
+			<div class="other">
+				<span class="member-core">发红包转赏金</span>
+				<span class="advertising"><i class="icon iconfont icon-arrow-right"></i></span>
+			</div>
+			<div class="other">
+				<span class="member-core">银行卡特惠</span>
+				<span class="advertising"><i class="icon iconfont icon-arrow-right"></i></span>
+			</div>
+			<div class="other">
+				<span class="member-core">娱乐宝</span>
+				<span class="advertising"><i class="icon iconfont icon-arrow-right"></i></span>
+			</div>
+			<div class="other">
+				<span class="member-core">帮助与反馈</span>
+				<span class="advertising">咨询票小蜜<i class="icon iconfont icon-arrow-right"></i></span>
+			</div>
+		</section>
 	</div>
 </template>
 
